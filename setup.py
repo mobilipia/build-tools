6a84cd843840e03d7ea8b4ad91fca3c5c78c9bd0
James Brady <james@jmsbrdy.com>
2011-08-09 11:34:14 -0400

    update requirements and add test command to setup

--- setup.py
+++ setup.py
@@ -4,28 +4,29 @@ import sys, os
 version = '0.1'
 
 setup(name='WebMynd Development Environment',
-    version=version,
-    description="",
-    long_description="""\
-    """,
-    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    keywords='',
-    author='James Brady',
-    author_email='james@webmynd.com',
-    url='http://www.webmynd.com/',
-    license='Closed source',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'requests', 'argparse'
-    ],
-    entry_points={
-        'console_scripts': [
-            'wm-refresh = webmynd.main:refresh',
-            'wm-init = webmynd.main:init',
-            'wm-dev-build = webmynd.main:development_build',
-            'wm-prod-build = webmynd.main:production_build',
-        ]
-    }
+	version=version,
+	description="",
+	long_description="""\
+	""",
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	keywords='',
+	author='James Brady',
+	author_email='james@webmynd.com',
+	url='http://www.webmynd.com/',
+	license='Closed source',
+	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+	include_package_data=True,
+	zip_safe=False,
+	test_suite="nose.collector",
+	setup_requires=['nose>=0.11'],
+	install_requires=[],
+	entry_points={
+		'console_scripts': [
+			'wm-refresh = webmynd.main:refresh',
+			'wm-init = webmynd.main:init',
+			'wm-dev-build = webmynd.main:development_build',
+			'wm-prod-build = webmynd.main:production_build',
+		]
+	}
 )
+
