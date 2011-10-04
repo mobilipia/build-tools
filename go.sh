ddbc8d42a309badf35f9a5c0c9b846981f636f5a
Tim Monks <t.r.monks@gmail.com>
2011-10-04 16:28:23 +0100

    dependencies in folders, not archive

--- go.sh
+++ go.sh
@@ -13,72 +13,40 @@ failure () {
 	exit
 }
 
+check_for_folder () {
+	if [ ! -e $1 ]
+	then
+		echo Cant find the $1 folder - you need to make sure you\'re running this script from the same folder that it\'s in
+		failure
+	fi
+}
+
+check_for_file () {
+	if [ ! -e $1 ]
+	then
+		echo Cant find file: $1 - you need to make sure you\'re running this script from the same folder that it\'s in
+		failure
+	fi
+}
+
 python -V >> $LOG_FILE 2>&1
 if [ $? -ne 0 ]
 then
 	echo 'Python not found.'
 	failure 
 fi
-echo 'python found.'
 
-if [ ! -e 'webmynd-environment' ]
-then
-	tar xzvf webmynd-environment.tar.gz >> $LOG_FILE 2>&1
-	if [ $? -ne 0 ]
-	then
-		echo
-		echo 'Extracting the virtual environment for python failed.'
-		failure
-	fi
-	echo 'WebMynd virtual environment extracted.'
-fi
+check_for_folder scripts
+check_for_folder webmynd-dependencies
+check_for_file scripts/activate
 
-. ./webmynd-environment/bin/activate
-
-if [ $? -ne 0 ]; then
-	rm -rf ./webmynd-environment
-	echo
-	echo 'Your virtual environment appears to be broken; please re-run this script to fix it!'
-	exit
-else
-	echo 'Entered WebMynd virtual env.'
-fi
-
-pip --version  >> $LOG_FILE 2>&1
+. ./scripts/activate
 if [ $? -ne 0 ]
 then
-	echo 'No pip, attempting install.'
-	easy_install pip
-	if [ $? -ne 0 ]
-	then
-		echo
-		echo 'Failed to install python package using easy_install.'
-		failure
-	fi
-	echo 'pip installed.'
+	echo 'The script to activate the virtual environment was there but something went wrong running it. You possibly need a fresh copy of the build tools.'
+	failure
 fi
-echo 'pip found.'
 
-echo 'Checking and installing requirements, this may take some time.'
-pip install -r requirements.txt >> $LOG_FILE 2>&1
-if [ $? -ne 0 ]
-then
-	echo 'Requirements install failed.'
-	failure 
-fi
-echo 'Requirements found and installed.'
-
-python setup.py install >> $LOG_FILE 2>&1
-if [ $? -ne 0 ]
-then
-	echo 'WebMynd setup failed.'
-	failure 
-fi
-echo 'WebMynd tools installed.'
-
-echo 'WebMynd environment ready, entering command line interface.'
-echo
-
-rm $LOG_FILE
+rm -f $LOG_FILE
 
 bash --norc
