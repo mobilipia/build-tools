a45ae3b2096d88e202ef769f166a72404d2a5dc6
Tim Monks <t.r.monks@gmail.com>
2011-10-07 14:50:55 +0100

    python script to create a zipfile containing forge-tools appropriate for users

---
+++ create_downloadable.py
@@ -0,0 +1,39 @@
+from zipfile import ZipFile
+
+import os
+from os import path
+import zipfile
+
+def add_folder_to_zip(folder, archive_path_prefix, zip):
+	for root, dirs, files in os.walk(folder):
+		for f in files:
+			path_to_file = path.join(root, f)
+			print "adding: ", path_to_file
+			archive_name = path.join(archive_path_prefix, path_to_file)
+
+			zip.write(path_to_file, archive_name)
+
+if __name__ == "__main__":
+
+	required_folders = [
+		'scripts',
+		'webmynd',
+		'webmynd-dependencies',
+	]
+
+	required_files = [
+		'go.sh',
+		'go.bat',
+		'common.sh',
+
+		'debug.keystore',
+		'README.rst',
+		'webmynd_build.json'
+	]
+
+	with ZipFile('forge-tools.zip', 'w') as distributable:
+		for f in required_folders:
+			add_folder_to_zip(f, 'forge-tools', distributable)
+
+		for r in required_files:
+			distributable.write(r, path.join('forge-tools', r))
