8b6deebbea65fd7d62d311374e22a13e21057562
Tim Monks <t.r.monks@gmail.com>
2011-11-24 13:39:53 +0000

    when packaged using pyinstaller, look for data files relative to the produced .exe file

--- webmynd/defaults.py
+++ webmynd/defaults.py
@@ -1,7 +1,21 @@
 'Project-wide default values'
+import sys
 from os import path
 
-FORGE_ROOT = path.abspath(path.join(__file__, "..", ".."))
+executable_name = path.basename(sys.executable)
+
+# this is a bit of a hack, the idea is:
+
+# if running from the non-packaged version (i.e. during development)
+if executable_name.startswith("python"):
+	# then look for data files relative to the unpackaged library files
+	FORGE_ROOT = path.abspath(path.join(__file__, "..", ".."))
+
+# else we're running the packaged forge.exe
+else:
+	# so look for data files relative to forge.exe
+	FORGE_ROOT = path.dirname(sys.executable)
+
 CONFIG_FILE = path.join(FORGE_ROOT, 'webmynd_build.json')
 PASSWORD = "your password"
 
