643fcab0e6d3bcbc418f9c2f39bba9e9870a29ac
Tim Monks <t.r.monks@gmail.com>
2011-11-25 09:52:47 +0000

    probably less fragile to check the sys.frozen attribute to decide whether running as a frozen app

--- webmynd/defaults.py
+++ webmynd/defaults.py
@@ -1,20 +1,17 @@
 'Project-wide default values'
 import sys
 from os import path
+import os
 
-executable_name = path.basename(sys.executable)
-
-# this is a bit of a hack, the idea is:
-
-# if running from the non-packaged version (i.e. during development)
-if executable_name.startswith("python"):
-	# then look for data files relative to the unpackaged library files
-	FORGE_ROOT = path.abspath(path.join(__file__, "..", ".."))
-
-# else we're running the packaged forge.exe
-else:
-	# so look for data files relative to forge.exe
+# if we're running the packaged forge.exe
+if 'frozen' in set(dir(sys)):
+	# then look for config file relative to forge.exe
 	FORGE_ROOT = path.dirname(sys.executable)
+
+# else we're running from the non-packaged version (i.e. during development)
+else:
+	# so look for config file relative to the unpackaged library files
+	FORGE_ROOT = path.abspath(path.join(__file__, "..", ".."))
 
 CONFIG_FILE = path.join(FORGE_ROOT, 'webmynd_build.json')
 PASSWORD = "your password"
