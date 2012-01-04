f5abce8990b6b11df96266f698fe2c2905c34102
Tim Monks <t.r.monks@gmail.com>
2012-01-04 13:55:12 +0000

    fix tests to deal with new commandline form 'forge foo'

--- forge/tests/test_main.py
+++ forge/tests/test_main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import mock
 from nose.tools import ok_, eq_, raises
 
@@ -10,7 +11,7 @@ from os import path
 @mock.patch('forge.main.logging')
 def _logging_test(args, level, logging):
 	main.setup_logging(args)
-	
+
 	logging.basicConfig.assert_called_once_with(level=getattr(logging, level),
 		format='[%(levelname)7s] %(message)s')
 	logging.getLogger.assert_called_once_with('forge.main')
@@ -50,20 +51,21 @@ class TestCreate(object):
 	@mock.patch('forge.main.os')
 	@mock.patch('forge.main.Remote')
 	@mock.patch('forge.main.argparse')
-	def test_normal(self, argparse, Remote, os, build_config):
+	def test_normal(self, argparse, Remote, mock_os, build_config):
+		mock_os.sep = os.sep
 		parser = argparse.ArgumentParser.return_value
 		parser.parse_args.return_value.name = None
 
-		os.path.exists.return_value = False
+		mock_os.path.exists.return_value = False
 		mock_raw_input = mock.MagicMock()
 		mock_raw_input.return_value = 'user input'
 		remote = Remote.return_value
 		build_config.load.return_value = dummy_config()
 
 		with mock.patch('__builtin__.raw_input', new=mock_raw_input):
-			main.create()
-		
-		os.path.exists.assert_called_once_with(defaults.SRC_DIR)
+			main.create([])
+
+		mock_os.path.exists.assert_called_once_with(defaults.SRC_DIR)
 		remote.create.assert_called_once_with(mock_raw_input.return_value)
 		remote.fetch_initial.assert_called_once_with(remote.create.return_value)
 
@@ -71,15 +73,16 @@ class TestCreate(object):
 	@mock.patch('forge.main.Remote')
 	@mock.patch('forge.main.argparse')
 	@raises(forge.ForgeError)
-	def test_user_dir_there(self, argparse, Remote, os):
+	def test_user_dir_there(self, argparse, Remote, mock_os):
+		mock_os.sep = os.sep
 		parser = argparse.ArgumentParser.return_value
-		os.path.exists.return_value = True
+		mock_os.path.exists.return_value = True
 		mock_raw_input = mock.MagicMock()
 		mock_raw_input.return_value = 'user input'
 		remote = Remote.return_value
 
 		with mock.patch('__builtin__.raw_input', new=mock_raw_input):
-			main.create()
+			main.create([])
 
 class TestRun(object):
 	@mock.patch('forge.main.build')
@@ -94,7 +97,7 @@ class TestRun(object):
 		args.production = False
 		parser.parse_args.return_value = args
 
-		main.run()
+		main.run([])
 
 		parser.parse_args.assert_called_once()
 
@@ -115,12 +118,12 @@ class TestRun(object):
 		def get_dir(*args, **kw):
 			return values.pop()
 
-		main.run()
-		
+		main.run([])
+
 		parser.parse_args.assert_called_once()
 		build.create_build.assert_called_once()
 		build.create_build.return_value.run.assert_called_once()
-	
+
 	@mock.patch('forge.main.build')
 	@mock.patch('forge.main.argparse')
 	@mock.patch('forge.main._assert_have_target_folder')
@@ -133,8 +136,8 @@ class TestRun(object):
 		args.sdk = 'sdk'
 		args.production = True
 		parser.parse_args.return_value = args
-		
-		main.run()
+
+		main.run([])
 
 		build.create_build.assert_called_once()
 		build.create_build.return_value.run.assert_called_once()
@@ -153,14 +156,14 @@ class TestRun(object):
 		args.production = True
 		parser.parse_args.return_value = args
 		build_config.load_app.return_value = {"name": "dummy name"}
-		
-		main.run()
+
+		main.run([])
 
 		build.create_build.assert_called_once()
 		build.create_build.return_value.run.assert_called_once()
 		build.import_generate_dynamic.assert_called_once()
 		build.import_generate_dynamic.return_value.customer_phases.run_ios_phase.assert_called_once()
-		
+
 class Test_AssertNotSubdirectoryOfForgeRoot(object):
 	@mock.patch('forge.main.os.getcwd')
 	@raises(main.RunningInForgeRoot)
@@ -195,18 +198,19 @@ class Test_AssertOutsideOfForgeRoot(object):
 
 class TestBuild(object):
 	def _check_common_setup(self, parser, Remote):
-		parser.parse_args.assert_called_once_with()
+		parser.parse_args.assert_called_once_with([])
 		args = parser.parse_args.return_value
 		args.quiet = False
 		main.setup_logging(args)
-	
+
 		Remote.assert_called_once_with(dummy_config())
-		
+
 	def _check_dev_setup(self, parser, Manager, Remote, Generate):
 		eq_(parser.add_argument.call_args_list,
 			[
-				(('-f', '--full'), {'action': 'store_true', 'help': 'Force a complete rebuild on the forge server'})
-			] + general_argparse)
+				(('-f', '--full'), {'action': 'store_true', 'help': 'Force a complete rebuild on the forge server'}),
+			]
+		)
 
 		Manager.assert_called_once_with(dummy_config())
 		Generate.assert_called_once_with(defaults.APP_CONFIG_FILE)
@@ -222,13 +226,13 @@ class TestBuild(object):
 	def test_user_dir_not_there(self, argparse, isdir, build_config):
 		isdir.return_value = False
 		build_config.load.return_value = dummy_config()
-		
-		main.development_build()
+
+		main.development_build([])
 
 		isdir.assert_called_once_with(defaults.SRC_DIR)
 		ok_(not build_config.called)
-		
-		
+
+
 	@mock.patch('forge.main.build_config')
 	@mock.patch('forge.main.os.path.isdir')
 	@mock.patch('forge.main.shutil')
@@ -242,11 +246,11 @@ class TestBuild(object):
 		args.full = False
 		isdir.return_value = True
 		build_config.load.return_value = dummy_config()
-		
-		main.development_build()
-		
+
+		main.development_build([])
+
 		self._check_dev_setup(parser, Manager, Remote, Generate)
-		
+
 		Manager.return_value.templates_for_config.assert_called_once_with(defaults.APP_CONFIG_FILE)
 		eq_(shutil.rmtree.call_args_list, [
 			(
@@ -260,7 +264,7 @@ class TestBuild(object):
 		])
 		shutil.copytree.assert_called_once_with(Manager.return_value.templates_for_config.return_value, 'development')
 		Generate.return_value.all.assert_called_once_with('development', defaults.SRC_DIR)
-		
+
 	@mock.patch('forge.main.build_config')
 	@mock.patch('forge.main.os.path.isdir')
 	@mock.patch('forge.main.shutil')
@@ -276,14 +280,14 @@ class TestBuild(object):
 		Remote.return_value.build.return_value = -1
 		isdir.return_value = True
 		build_config.load.return_value = dummy_config()
-		
-		main.development_build()
-		
+
+		main.development_build([])
+
 		self._check_dev_setup(parser, Manager, Remote, Generate)
 		Manager.return_value.templates_for_config.assert_called_once_with(defaults.APP_CONFIG_FILE)
 		Remote.return_value.build.assert_called_once_with(development=True, template_only=True)
 		Manager.return_value.fetch_templates.assert_called_once_with(Remote.return_value.build.return_value, clean=False)
-		
+
 		eq_(shutil.rmtree.call_args_list, [
 			(
 				('development',),
@@ -296,7 +300,7 @@ class TestBuild(object):
 		])
 		shutil.copytree.assert_called_once_with(Manager.return_value.fetch_templates.return_value, 'development')
 		Generate.return_value.all.assert_called_once_with('development', defaults.SRC_DIR)
-		
+
 	@mock.patch('forge.main.build_config')
 	@mock.patch('forge.main.os.path.isdir')
 	@mock.patch('forge.main.Remote')
@@ -306,11 +310,11 @@ class TestBuild(object):
 		Remote.return_value.build.return_value = -1
 		isdir.return_value = True
 		build_config.load.return_value = dummy_config()
-		
-		main.production_build()
-		
+
+		main.production_build([])
+
 		self._check_prod_setup(parser, Remote)
-		
+
 		Remote.return_value.build.assert_called_once_with(development=False, template_only=False)
 		Remote.return_value.fetch_unpackaged.assert_called_once_with(Remote.return_value.build.return_value, to_dir='production')
 
@@ -321,7 +325,7 @@ class TestWithErrorHandler(object):
 	def test_keyboard_interrupt(self, sys, warn_if_subdir, assert_outside):
 		def interrupt():
 			raise KeyboardInterrupt()
-		
+
 		main.with_error_handler(interrupt)()
-		
+
 		sys.exit.assert_called_once_with(1)
