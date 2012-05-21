a0ad0377da51c2a0fce1ed0d8e67b5b4aeb275f9
James Brady <james.colin.brady@gmail.com>
2012-05-21 15:34:32 +0100

    forge refresh of generate_dynamic after downloading new

--- forge/main.py
+++ forge/main.py
@@ -410,7 +410,6 @@ def run(unhandled_args):
 	generate_dynamic.customer_goals.run_app(
 		generate_module=generate_dynamic,
 		build_to_run=build_to_run,
-		server=False,
 	)
 
 def package(unhandled_args):
@@ -426,7 +425,6 @@ def package(unhandled_args):
 	generate_dynamic.customer_goals.package_app(
 		generate_module=generate_dynamic,
 		build_to_run=build_to_run,
-		server=False,
 	)
 
 def check(unhandled_args):
